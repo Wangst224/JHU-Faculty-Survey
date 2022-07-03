7a4d367087761d0a9ce0bf38ac005ba116214055
Shengtao Wang <wangst224@gmail.com>
2022-07-03 15:43:00 -0400

    Update app.R

--- app.R
+++ app.R
@@ -42,6 +42,9 @@ ui = dashboardPage(
                                 'Ethnicity' = 'ethnicity')
                 ),
                 
+                textInput(inputId = 'testtext', label = 'Input text here'),
+                textOutput('textout'),
+                
                 actionButton(inputId = 'update', label = 'Update')
             )
         ),
@@ -58,11 +61,11 @@ ui = dashboardPage(
                 box(plotOutput('histogram'))
             ),
             
-            tabItem(tabName = 'Q1b', 'This is tab1b'),
+            tabItem(tabName = 'Q1b', 'This is tab1b',textOutput('textout1'),),
             
-            tabItem(tabName = 'Q2a', 'This is tab2a'),
+            tabItem(tabName = 'Q2a', 'This is tab2a',textOutput('textout2'),),
             
-            tabItem(tabName = 'Q2b', 'This is tab2b')
+            tabItem(tabName = 'Q2b', 'This is tab2b',textOutput('textout3'),)
         )
     )
 )
@@ -72,6 +75,13 @@ server = function(input, output){
             ggplot(aes(x), data = data.frame(x = rnorm(100))) +
                 geom_histogram()
         })
+    
+    ttt = eventReactive(input$update, {input$testtext})
+    
+    output$textout = renderText(ttt)
+    output$textout1 = renderText(paste('1. Here is the text: ', ttt))
+    output$textout2 = renderText(paste('2. Here is the text: ', ttt))
+    output$textout3 = renderText(paste('3. Here is the text: ', ttt))
 }
 
 shinyApp(ui = ui, server = server)
